2a1acd7557263a1d784f8afd40f179df3e4afc4c
Nauman Khan <thetopofsoup@gmail.com>
2024-06-04 15:08:12 -0400

    snake

--- snake/snake.js
+++ snake/snake.js
@@ -7,6 +7,18 @@ let cols = 20
 let board 
 let context
 
+
+//snake head
+let snakeX = blockSize * 5
+let snakeY = blockSize * 5
+
+
+//food
+let foodX = blockSize * 10
+let foodY = blockSize * 10
+
+
+
 window.onload = function() {
     board = document.getElementById("board")
     board.height = rows * blockSize
@@ -19,4 +31,10 @@ window.onload = function() {
 function update() {
     context.fillStyle="black"
     context.fillRect(0, 0, board.width, board.height)
+
+    context.fillStyle="lime"
+    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+
+    context.fillStyle="Red"
+    context.fillRect(foodX, foodY, blockSize, blockSize)
 }
