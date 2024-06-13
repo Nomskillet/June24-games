e6838582c4394844a4cb6e9e91bd6107e4bfb3f6
Nauman Khan <thetopofsoup@gmail.com>
2024-06-13 15:41:24 -0400

    TTT

--- TTT/script.js
+++ TTT/script.js
@@ -1,35 +1,35 @@
 let board;
-let playerO = "O"
-let playerX = "X"
-let currPlayer = playerO
-let gameOver = false
+let playerO = "O";
+let playerX = "X";
+let currPlayer = playerO;
+let gameOver = false;
 
 
 window.onload = function() {
-    setGame()
+    setGame();
 }
 
 
 function setGame() {
     board = [
+        ['', '', ''],
+        ['', '', ''],
         ['', '', '']
-        ['', '', '']
-        ['', '', '']
-    ]
+    ];
 
     for (let r = 0; r < 3; r++ ) {
-        for (let c = 0; c < 0; c++) {
-
+        for (let c = 0; c < 3; c++) {
             let tile = document.createElement("div")
             tile.id = r.toString() + "-" + c.toString();
-            tile.classList.add("tile")
+            tile.classList.add("tile");
             if (r == 0 || r == 1) {
-                tile.classList.add("horizontal-line")
+                tile.classList.add("horizontal-line");
             }
 
             if (c == 1 || c == 0 ) {
-                tile.classList.add("vertical-line")
+                tile.classList.add("vertical-line");
             }
+            document.getElementById("board").append(tile)
         }
     }
 }
